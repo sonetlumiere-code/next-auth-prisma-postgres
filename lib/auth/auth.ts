18a8a0b1297a0d48759268606d4d44766d44c5a8
sonetlumiere-code <lucas.perez.ok@gmail.com>
2024-06-08 00:22:28 -0300

    user role & typescript module augmentation

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -2,18 +2,33 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/lib/db/db"
 import authConfig from "./auth.config"
+import { Role } from "@prisma/client"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
     async jwt({ token }) {
-      token.customField = "test"
-      console.log(token)
+      if (!token.sub) return token
+
+      const existingUser = await prisma.user.findUnique({
+        where: { id: token.sub },
+      })
+
+      if (!existingUser) return token
+
+      token.role = existingUser.role
+
       return token
     },
+
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub
       }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as Role
+      }
+
       return session
     },
   },
